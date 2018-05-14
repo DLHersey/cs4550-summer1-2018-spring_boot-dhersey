8da4e63bff3002a681107fe21ca4dabf7a9d5972
David Hersey <hersey.da@husky.neu.edu>
2018-05-14 15:48:17 -0700

    created user client service

--- src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
+++ src/main/webapp/jquery/components/user-admin/user-admin.controller.client.js
@@ -1,18 +1,113 @@
 (function () {
-    var $usernameFld, $passwordFld;
-    var $removeBtn, $editBtn, $createBtn;
-    var $firstNameFld, $lastNameFld;
-    var $userRowTemplate, $tbody;
-    var userService = new AdminUserServiceClient();
+
+    var tbody;
+    var template;
+    var userService = new UserServiceClient()
+
     $(main);
 
-    function main() { … }
-    function createUser() { … }
-    function findAllUsers() { … }
-    function findUserById() { … }
-    function deleteUser() { … }
-    function selectUser() { … }
-    function updateUser() { … }
-    function renderUser(user) { … }
-    function renderUsers(users) { … }
+    function main() { 
+        //Retrieved the dom elements needed later in the controller such as the form elements, action icons, and templates.
+        //Binds action icons, such as create, update, select, and delete, to respective event handlers
+
+
+        tbody = $('tbody');
+        template = $('.userRowTemplate');
+
+
+        //user list
+        var promise = fetch('/api/user');
+        promise.then(function (response) {
+            return response.json();
+            }).then(renderUsers)
+
+        //Handle form submit button.
+        $('#createUser').click(createUser());
+
+        renderUsers();
+        findAllUsers();
+    }
+    
+    function createUser() {
+        console.log('createUser');
+
+        var username = $('#usernameFld').val();
+        var password = $('#passwordFld').val();
+        var firstName = $('#firstNameFld').val();
+        var lastName = $('#lastNameFld').val();
+        var role = $('#roleFld').val();
+
+        var user = {
+            username: username,
+            password: password,
+            firstName: firstName,
+            lastName: lastName,
+            role: role
+        };
+
+        userService
+            .createUser(user)
+            .then(findAllUsers);
+    }
+    
+    function findAllUsers() { 
+        userService
+            .findAllUsers()
+            .then(renderUsers);
+    }
+    
+    function findUserById() { 
+
+    }
+
+    function selectUser() {
+
+    }
+    
+    function updateUser() {
+        //Posts user information from the form to the database and triggeres a renderUsers.
+
+        //renderUsers(users);
+    }
+    
+    function renderUser(user) {
+        //populates form with a user's information pulled from the server.
+    }
+    
+    function renderUsers(users) {
+        // clear current user list and populates with users list.
+        tbody.empty();
+        for(var i=0; i<users.length; i++) {
+            var user = users[i];
+            var clone = template.clone();
+
+            clone.attr('id', user.id);
+
+            clone.find('.delete').click(deleteUser);
+            clone.find('.edit').click(editUser);
+
+            clone.find('.username')
+                .html(user.username);
+            tbody.append(clone);
+        }
+    }
+
+
+    function deleteUser(event) {
+        var deleteBtn = $(event.currentTarget);
+        var userId = deleteBtn
+            .parent()
+            .parent()
+            .attr('id');
+
+        userService
+            .deleteUser(userId)
+            .then(findAllUsers);
+    }
+
+    function editUser(event) {
+        console.log('editUser');
+        console.log(event);
+    }
+
 })();
